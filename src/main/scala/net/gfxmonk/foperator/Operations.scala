96fb9b2f88307e809cba8cad962e0d022838afbc
Tim Cuthbertson <tim@gfxmonk.net>
2020-07-18 09:48:05 +1000

    minimal updates

--- src/main/scala/net/gfxmonk/foperator/Operations.scala
+++ src/main/scala/net/gfxmonk/foperator/Operations.scala
@@ -14,6 +14,15 @@ object Update {
   case class Metadata[T](initial: T, metadata: ObjectMeta) extends Update[T, Nothing]
   case class None[T](value: T) extends Update[T,Nothing]
 
+  // TODO can we use something more general than CustomResource
+  def minimal[Sp,St](update: Update[CustomResource[Sp,St], St]): Update[CustomResource[Sp,St], St] = {
+    update match {
+      case Status(initial, status) => if(initial.status == status) None(initial) else update
+      case Metadata(initial, metadata) => if(initial.metadata == metadata) None(initial) else update
+      case None(initial) => None(initial)
+    }
+  }
+
   object Implicits {
     implicit class UpdateExt[T<:ObjectResource](val resource: T) extends AnyVal {
       def statusUpdate[St](st: St): Update[T,St] = Update.Status(resource, st)
@@ -46,8 +55,9 @@ object Operations {
     st: HasStatusSubresource[CustomResource[Sp,St]],
     client: KubernetesClient
   ): Task[CustomResource[Sp,St]] = {
-    implicit val hasStatus: HasStatusSubresource[CustomResource[Sp,St]] = st // TODO why is this needed?
-    Task.deferFuture(update match {
+    type Resource = CustomResource[Sp,St]
+    implicit val hasStatus: HasStatusSubresource[Resource] = st // TODO why is this needed?
+    Task.fromFuture(Update.minimal(update) match {
       // TODO no updateMetadata? Is metadata not a subresource?
       case Update.Metadata(original, meta) => client.update(original.withMetadata(meta))
       case Update.Status(original, st) => client.updateStatus(original.withStatus(st))
